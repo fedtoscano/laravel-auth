a6ff08a95da7932ff75e7e86a66e589e1f4f8d3b
fedtoscano <federicotoscano611@gmail.com>
2024-07-29 14:41:16 +0200

    creata la index di tutti i projects

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -3,7 +3,42 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        @dump($projects)
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Project Name</th>
+                <th scope="col">Client</th>
+                <th scope="col">Stack</th>
+                <th scope="col">Data di inizio</th>
+                <th scope="col">Stato</th>
+                <th scope="col">Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach ($projects as $project)
+            <tr>
+                <th scope="row">{{$project->id}}</th>
+                <td>{{$project->name}}</td>
+                <td>{{$project->client}}</td>
+                <td>
+                    @php
+                    $techStack = json_decode($project->tech_stack);
+                    @endphp
+                        @foreach ($techStack as $singleTechStack)
+                            <span>{{$singleTechStack}}</span>
+                        @endforeach</td>
+                <td>{{$project->start_date}}</td>
+                <td>{{($project->status) ? "In corso" : "Completato"}}</td>
+                <td>
+                    <button>View</button>
+                    <button>Edit</button>
+                    <button>Delete</button>
+                </td>
+            </tr>
+            @endforeach
+            </tbody>
+        </table>
     </div>
 </div>
 @endsection
